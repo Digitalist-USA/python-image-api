9bfe66c9f41c304a72fcccaa7c67fc180796242d
Jenni <jenni.hantula@gmail.com>
2018-04-18 14:18:49 -0700

    Add custom error handling

--- endpoints/is_cat.py
+++ endpoints/is_cat.py
@@ -16,6 +16,29 @@ def is_cat():
     if request.method == "GET":
         LOGGER.info("Received a GET request")
         return jsonify({"status": 200, "message": "OK"})
-    else:
-        LOGGER.warning("Request method %s unimplemented", request.method)
-        return jsonify({"status": 405, "message": "Unimplemented method"}), 405
+    LOGGER.warning("Request method %s unimplemented", request.method)
+    return jsonify({"status": 405, "message": "Unimplemented method"}), 405
+
+
+@APP.errorhandler(404)
+def not_found(error):
+    """
+    Custom error handling (404)
+
+    Args:
+    Returns:
+    """
+    LOGGER.warning(error)
+    return jsonify({"status": 404, "message": "Not found"}), 404
+
+
+@APP.errorhandler(500)
+def server_error(error):
+    """
+    Custom error handling (500)
+
+    Args:
+    Returns:
+    """
+    LOGGER.error("Unexpected exception: %s", error)
+    return jsonify({"status": 500, "message": "Server error"}), 500
