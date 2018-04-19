c1e2e50c86007a460735140ba10c8354ae060b69
Jenni <jenni.hantula@gmail.com>
2018-04-19 13:45:10 -0700

    Remove ORM-related imports/objects (will not use)

--- start.py
+++ start.py
@@ -5,7 +5,6 @@ import logging
 
 from flask import Flask
 from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
 
 APP = Flask(__name__)
 
@@ -14,17 +13,8 @@ logging.basicConfig(level="INFO")
 
 # TODO maybe move to a database module
 ENGINE = create_engine('sqlite:////tmp/cat.db', convert_unicode=True)
-DB_SESSION = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=ENGINE))
 
 # FIXME circular import
 # pylint: disable=unused-import
 # pylint: disable=wrong-import-position
 import endpoints.is_cat
-
-
-# pylint: disable=unused-argument
-@APP.teardown_appcontext
-def shutdown_session(exception=None):
-    DB_SESSION.remove()
