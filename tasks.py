47864febc37f588ce268f7106b825eb8fe57eb80
Jenni <jenni.hantula@gmail.com>
2018-04-20 14:46:40 -0700

    Return status message from task

--- tasks.py
+++ tasks.py
@@ -1,9 +1,14 @@
 """
 Celery tasks
 """
+import logging
+
 from celery import Celery
 
 from service.cat_service import CAT_SERVICE
+
+
+LOGGER = logging.getLogger(__name__)
 
 
 CELERY_APP = Celery("tasks", broker="redis://localhost:6379/0")
@@ -21,6 +26,12 @@ def process_and_save(image_url):
         image_url: valid url to image
 
     Returns:
-
+        str:
     """
-    CAT_SERVICE.save_cat(image_url)
+    try:
+        CAT_SERVICE.save_cat(image_url)
+        return f"Successfully processed {image_url}"
+    # pylint: disable=broad-except
+    except Exception as ex:
+        LOGGER.exception(ex)
+        return f"Failed to process {image_url}"
