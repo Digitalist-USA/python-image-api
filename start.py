1875aaf8ce9163d2a94f0a5072fc8e25b0f7995f
Jenni <jenni.hantula@gmail.com>
2018-04-20 14:59:25 -0700

    Fix linting errors

--- start.py
+++ start.py
@@ -7,10 +7,8 @@ from flask import Flask
 
 APP = Flask(__name__)
 
-# TODO logging configuration
 logging.basicConfig(level="INFO")
 
-# FIXME circular import
 # pylint: disable=unused-import
 # pylint: disable=wrong-import-position
 import endpoints.is_cat
